0b580d37ceb59c94bf908c0170f5805ed3c7f1f1
Juan Santos <juan.santos@unosquare.com>
2022-04-06 13:22:34 -0500

    Creating first custom element: ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import ExpenseItem from './components/ExpenseItem';
+  
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,9 +9,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <ExpenseItem />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,17 @@
+
+function ExpenseItem(props) {
+
+  return(
+    <div>
+      <div>
+        {props.title}
+      </div>
+      <div>
+        {props.amount}
+      </div>
+    </div>
+  );
+
+}
+
+export default ExpenseItem;
