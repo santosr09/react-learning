c794bc4813b8bedb46e72e1d208916f1ab07551e
Juan Santos <santos.r09@gmail.com>
2022-04-07 12:09:57 -0500

    Solving issue with style in ExpenseItem

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
+import './ExpenseItem.css'
+
 const ExpenseItem = (props) => {
   const [title, setTitle] = useState(props.title);
 
