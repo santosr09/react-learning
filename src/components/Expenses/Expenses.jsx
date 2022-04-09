e38cf893c9bf5973cf2eb5fda75186512b27ee76
Juan Santos <santos.r09@gmail.com>
2022-04-08 20:05:28 -0500

    Dynamic styling

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+import './Expenses.css';
 
 const Expenses = (props) => {  
 
@@ -19,6 +22,7 @@ const Expenses = (props) => {
   return (
     <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses}/>
     </Card>
   );
