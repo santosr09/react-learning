50375ac8f257205e337abc8747e8e999ec1afa62
Juan Santos <santos.r09@gmail.com>
2022-04-11 11:19:28 -0500

    Working with Fragments, Portals & Refs
    -Understanding and using Wrapper components
    -Using React Fragments.

--- src/App.js
+++ src/App.js
@@ -1,40 +1,26 @@
-import Expenses from './components/Expenses/Expenses'; 
+import React, { useState } from 'react';
 
-import ITEMS from './dummy-data.json'
-
-import logo from './logo.svg';
-import './App.css';
-import NewExpense from './components/NewExpense/NewExpense';
-import { useState } from 'react';
-
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const [ expenses, setExpenses ] = useState(ITEMS);
-
-  const addExpenseHandler = (expense) => {
-    //Updating an state that depends on previous state
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
-  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
-    </div>
+    // Using React Fragment
+    <>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </>
   );
 }
 
