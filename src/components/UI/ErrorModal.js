f53b60c0b036fe3ef66a88eb951286d90ef3cea6
Juan Santos <santos.r09@gmail.com>
2022-04-11 15:51:52 -0500

    using portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
+  console.log("props.onConfirm): "+props.onConfirm);
+  console.log("props.onClick): "+props.onClick);
+
+  // Using Portals
+  const Backdrop = (props) => {
+    return <div className={classes.backdrop} onClick={props.onClick} />;
+  }
+
+  // Using portals
+  const ModalOverlay = (props) => {
+    return (
       <Card className={classes.modal}>
         <header className={classes.header}>
           <h2>{props.title}</h2>
@@ -16,11 +25,42 @@ const ErrorModal = (props) => {
           <p>{props.message}</p>
         </div>
         <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
+          <Button onClick={props.onClick}>Okay</Button>
         </footer>
       </Card>
-    </div>
+    )
+  }
+
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(<Backdrop 
+      onClick={props.onClick} />, 
+      document.getElementById("backdrop-root"))}
+      
+      {ReactDOM.createPortal(<ModalOverlay 
+      title={props.title} message={props.message} onClick={props.onClick} />, 
+      document.getElementById("modal-root"))}
+    </React.Fragment>
   );
+
+  // Withouth using PORTALs
+  /*  return (
+     <div>
+       <div className={classes.backdrop} onClick={props.onConfirm} />
+       <Card className={classes.modal}>
+         <header className={classes.header}>
+           <h2>{props.title}</h2>
+         </header>
+         <div className={classes.content}>
+           <p>{props.message}</p>
+         </div>
+         <footer className={classes.actions}>
+           <Button onClick={props.onConfirm}>Okay</Button>
+         </footer>
+       </Card>
+     </div>
+   ); */
+
 };
 
 export default ErrorModal;
